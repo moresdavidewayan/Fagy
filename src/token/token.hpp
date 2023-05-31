c80a93dc3be06f66bbed3444de0a27663e4deca1
Davide Wayan Mores <ilfagianodorato@gmail.com>
2023-05-31 09:25:08 +0200

    Add new token types

--- src/token/token.hpp
+++ src/token/token.hpp
@@ -3,7 +3,19 @@
 #include <string>
 
 namespace fagy {
-enum class TokenType { TOKEN_EOF };
+enum class TokenType {
+  TOKEN_EOF,
+
+  // \n \t
+  TOKEN_NEW_LINE,
+  TOKEN_INDENTATION,
+
+  // ()[]
+  TOKEN_LEFT_PARENTHESIS,
+  TOKEN_RIGHT_PARENTHESIS,
+  TOKEN_LEFT_BRACKET,
+  TOKEN_RIGHT_BRACKET
+};
 
 class Token {
   TokenType type;
